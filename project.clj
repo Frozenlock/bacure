75df6043fcf6fc9f79e507eecfc751633d6a7c6a
Frozenlock <frozenlock@gmail.com>
2013-02-24 17:37:10 -0500

    up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bacure "0.1.13"
+(defproject bacure "0.1.14"
   :description "A Clojure wrapper for the BAC4j library"
   :url "https://bacnethelp.com"
   :license {:name "GNU General Public License V3"
