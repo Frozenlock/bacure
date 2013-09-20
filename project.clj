3c6fc2cd58394e7a483be262dba92697ab669b8e
Frozenlock <frozenlock@gmail.com>
2013-09-19 23:15:55 -0400

    new version for clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bacure "0.3.16"
+(defproject bacure "0.3.17"
   :description "A Clojure wrapper for the BAC4j library"
   :url "https://bacnethelp.com"
   :license {:name "GNU General Public License V3"
