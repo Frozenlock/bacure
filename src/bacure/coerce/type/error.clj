58570398c537ce4835974c24c2b6ef60de44ecc1
Frozenlock <frozenlock@gmail.com>
2016-07-03 20:09:05 -0400

    add forgotten error.clj file

---
+++ src/bacure/coerce/type/error.clj
@@ -0,0 +1,12 @@
+(ns bacure.coerce.type.error
+  (:require [bacure.coerce :as c :refer [bacnet->clojure clojure->bacnet]]
+            [bacure.coerce.type.constructed :as ct])
+  (:import [com.serotonin.bacnet4j.type.error
+            BaseError]))
+
+
+;; ONE WAY
+
+(defmethod bacnet->clojure BaseError
+  [^BaseError o]
+  (.getError o))
