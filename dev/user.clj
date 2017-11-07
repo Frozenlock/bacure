c78d03cf8109ada46681d95dc31e684223720f66
Alex Whitt <alex.joseph.whitt@gmail.com>
2017-11-07 16:28:39 -0500

    Adding dev/user.clj for REPL help

---
+++ dev/user.clj
@@ -0,0 +1,8 @@
+(ns user
+  (:require [bacure.remote-device :as bc-remote]
+            [bacure.local-device :as bc-local]
+            [bacure.core :as bc-core]
+            [bacure.read-properties :as bc-read]
+            [serial.core]
+            [serial.util]))
+
