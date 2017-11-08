3a21561b8e11f692c79d0c647984326e80071080
Alex Whitt <alex.joseph.whitt@gmail.com>
2017-11-08 13:52:34 -0500

    Add logging configuration and dependency for SLF4J using log4j

--- project.clj
+++ project.clj
@@ -8,8 +8,9 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.serotonin/bacnet4j "4.0.1"]
                  [clj-serial "2.0.3"]
-                 ;[org.clojars.frozenlock/bacnet4j "3.2.4-4"]
-                 [clj-time "0.12.0"]]
+                 ;;[org.clojars.frozenlock/bacnet4j "3.2.4-4"]
+                 [clj-time "0.12.0"]
+                 [org.slf4j/slf4j-log4j12 "1.8.0-beta0"]]
   :repl-options {:init-ns user}
   :plugins [[lein-codox "0.9.5"]]
   :codox {:doc-paths ["docs"]
