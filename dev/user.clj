f38280767bcda7f6c258a2671341392e0425b864
Alex Whitt <alex.joseph.whitt@gmail.com>
2017-12-20 18:09:11 -0500

    Useful :requires

--- dev/user.clj
+++ dev/user.clj
@@ -3,6 +3,8 @@
             [bacure.local-device :as ld]
             [bacure.core :as bc-core]
             [bacure.read-properties :as bc-read]
+            [bacure.services :as services]
+            [clojure.repl :refer :all]
             [serial.core]
             [serial.util]))
 
