fe004d41f36605af54c3db323ce8f062954b6597
Alex Whitt <alex.joseph.whitt@gmail.com>
2017-12-20 18:21:45 -0500

    Also useful to require

--- dev/user.clj
+++ dev/user.clj
@@ -4,6 +4,7 @@
             [bacure.core :as bc-core]
             [bacure.read-properties :as bc-read]
             [bacure.services :as services]
+            [bacure.events :as events]
             [clojure.repl :refer :all]
             [serial.core]
             [serial.util]))
