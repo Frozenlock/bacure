b6db9be4e1f787e667df876f21538f143c6fa650
Alex Whitt <alex.joseph.whitt@gmail.com>
2017-12-21 15:40:37 -0500

    Remove

--- src/bacure/remote_device.clj
+++ src/bacure/remote_device.clj
@@ -215,7 +215,6 @@
   ([] (discover-network nil))
   ([local-device-id] (discover-network local-device-id 5))
   ([local-device-id tries]
-                                        ;(events/clear-cached-remote-devices!)
    (dorun
     (->> (repeatedly tries #(find-remote-devices-and-extended-information local-device-id {}))
          (take-while empty?)))
