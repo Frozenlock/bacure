1e98f5ff06d9c6a316b911217b82d21295e7387d
Alex Whitt <alex.joseph.whitt@gmail.com>
2017-12-21 14:39:03 -0500

    Useful to access atoms quickly

--- dev/user.clj
+++ dev/user.clj
@@ -5,6 +5,7 @@
             [bacure.read-properties :as bc-read]
             [bacure.services :as services]
             [bacure.events :as events]
+            [bacure.state :as state]
             [clojure.repl :refer :all]
             [serial.core]
             [serial.util]))
