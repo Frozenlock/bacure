9b3d02cb7a4ef61730e56b9458373aa36872801f
Alex Whitt <alex.joseph.whitt@gmail.com>
2017-12-22 15:54:19 -0500

    Move mapify into generic util NS so it can be used elsewhere

--- src/bacure/local_device.clj
+++ src/bacure/local_device.clj
@@ -14,25 +14,6 @@
                                    npdu.ip.IpNetwork
                                    transport.DefaultTransport)))
 
-
-
-(defmacro mapify
-  "Given some symbols, construct a map with the symbols as keys, and
-  the value of the symbols as the map values. For example:
-  (Let [aa 12]
-     (mapify aa))
-  => {:aa 12}"
-  [& symbols]
-  `(into {}
-         (filter second
-                 ~(into []
-                        (for [item symbols]
-                          [(keyword item) item])))))
-
-
-;; ================
-
-
 ;; we store all the local devices with their device-id as the key.
 
 (defn list-local-devices []
