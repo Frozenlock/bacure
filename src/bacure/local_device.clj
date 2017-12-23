e867dc2833204be5d3a9075d273247185ff8093d
Frozenlock <frozenlock@gmail.com>
2017-12-23 14:54:06 -0500

    Make sure we require bacure.coerce.type.error

--- src/bacure/local_device.clj
+++ src/bacure/local_device.clj
@@ -5,6 +5,7 @@
             [bacure.coerce.type.primitive]
             [bacure.coerce.type.enumerated]
             [bacure.coerce.type.constructed]
+            [bacure.coerce.type.error]
             [bacure.local-save :as save]
             [bacure.serial-connection :as serial]
             [bacure.state :as state]
