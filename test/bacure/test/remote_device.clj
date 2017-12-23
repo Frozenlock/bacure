248dd9bdff22f35847c73e7dc5012f04a4a7492c
Frozenlock <frozenlock@gmail.com>
2017-12-23 15:09:05 -0500

    Tests are now local (no need for another BACnet machine)
    We are leveraging the new 'register-as-foreign-device' to create two
    local devices on different IPs (127.0.0.1/2) and link them as foreign
    devices.
    
    We still can't test correctly for creating and deleting objects...
    Perhaps we need to support those manually?

--- test/bacure/test/remote_device.clj
+++ test/bacure/test/remote_device.clj
@@ -3,23 +3,29 @@
   (:require [bacure.local-device :as ld]
             [bacure.read-properties :as rp]
             [bacure.remote-device :as rd]
+            [bacure.test.foreign-device :as fd]
             [bacure.services :as services]
             [bacure.network :as net]))
 
+(defn init-local-test-devices!
+  "Boot up 2 local devices with ID 1 and 2. The devices register as
+  foreign devices to each other." []
+  ;; generate local devices 1 and 2
+  (fd/generate-local-devices! 2)
+  ;; make device 2 aware of device 1
+  (ld/register-as-foreign-device 1 "127.0.0.2" 47808 60)
+  (ld/register-as-foreign-device 2 "127.0.0.1" 47808 60)
+  (ld/i-am-broadcast! 1)
+  (ld/i-am-broadcast! 2)
+  (Thread/sleep 50))
 
-(defn init-test-local-device! [local-device-id]
-  ;; adjust the broadcast-address as needed.
-  (ld/new-local-device! {:device-id local-device-id :broadcast-address
-                         (-> (net/get-any-ip)
-                             (net/get-broadcast-address))})
-  (ld/initialize! local-device-id))
 
 
 (deftest basic-read-properties
   (ld/with-temp-devices
-    (let [ld-id 1133
-          rd-id 1338]
-      (init-test-local-device! ld-id)
+    (init-local-test-devices!)
+    (let [ld-id 1
+          rd-id 2]
       (testing "Partition array"
         (with-redefs [services/send-request-promise (constantly {:success 10})]
           (let [expected-result (into [] (for [i (range 1 11)]
@@ -27,101 +33,96 @@
             (is (= expected-result 
                    (rp/expand-array ld-id rd-id [:device rd-id] :object-list)))))))))
 
+
 (deftest read-properties
   (ld/with-temp-devices
-    (let [ld-id 1133  ;; test local device-id
-          rd-id 1338]
-      ;; first we create a local device
-      (init-test-local-device! ld-id)
-      (is (some #{rd-id} (rd/discover-network ld-id)) 
+    ;; first we create the local devices
+    (init-local-test-devices!)
+    (let [ld-id 1
+          rd-id 2]
+      (is (some #{rd-id} (rd/remote-devices ld-id))
           (str "This test requires a device with ID "rd-id " on the network."))
-      ;(println (rp/read-properties ld-id rd-id [[[:device rd-id] :object-name]]))
+      
       (testing "Read properties" 
         ;; after discovering the network, we should have remote device
         ;; extended information (mostly to know if we can use
         ;; readPropertyMultiple)."
         (is (= (get-in (rd/extended-information ld-id rd-id) 
                        [:protocol-services-supported :read-property])
-                 true))
+               true))
         ;; we try a few reads
-        
-        (testing "Read individually"
-          ;; Reading individually means that each
-          ;; object-property-reference is requested separately, then
-          ;; merge into objects maps.
 
-          (let [{:keys [object-name protocol-version protocol-revision] :as result}
-                (first (rp/read-individually ld-id rd-id [[[:device rd-id] :object-name]
-                                                          [[:device rd-id] :protocol-version]
-                                                          [[:device rd-id] :protocol-revision]
-                                                          [[:device rd-id] [:object-list 1]]]))] ;; <--- with array index
-            (is (and object-name protocol-version protocol-revision))
-            (is (= [:device rd-id] (some-> result (get :object-list))))))
+        (let [obj-prop-references [[[:device rd-id] :object-name]
+                                   [[:device rd-id] :protocol-version]
+                                   [[:device rd-id] :protocol-revision]
+                                   [[:device rd-id] [:object-list 1]]]] ; <--- with array index
+          (testing "Read individually"
+            ;; Reading individually means that each
+            ;; object-property-reference is requested separately, then
+            ;; merge into objects maps.
+            (let [indiv-read (rp/read-individually ld-id rd-id obj-prop-references)
+                  {:keys [object-name protocol-version protocol-revision] :as result} (first indiv-read)]
+              (is (and object-name protocol-version protocol-revision))
+              (is (= [:device rd-id] (some-> result (get :object-list))))
 
-        (testing "Read Property Multiple"
-          (let [{:keys [object-name protocol-version protocol-revision object-list] :as results}
-                (first (rp/read-property-multiple ld-id rd-id [[[:device rd-id] :object-name]
-                                                               [[:device rd-id] :protocol-version]
-                                                               [[:device rd-id] [:object-list 6]]]))]
-            (is (and object-name protocol-version object-list))))
-        
-        (let [{:keys [object-name protocol-version]}
-              (first
-               (rp/read-properties ld-id rd-id [[[:device rd-id] :object-name :protocol-version :protocol-revision]]))]          
-          (is (and object-name
-                   protocol-version))))
+              (testing "Read Property Multiple"
+                (let [multi-read (rp/read-property-multiple ld-id rd-id obj-prop-references)                      
+                      {:keys [object-name protocol-version protocol-revision object-list] :as results}
+                      (first multi-read)]
+                  ;; result of reading individually or in bulk should be the same
+                  (is (= indiv-read multi-read))))))))
 
-      ;; (testing "Create/delete object"
-      ;;   ;; first we delete the object to make sure it isn't there.
-      ;;   (rd/delete-remote-object! ld-id rd-id [:analog-input 1])
+      (testing "Create/delete object"
+        ;; first we delete an objects that doesn't exist. We should get an error
+        ;; ;; then we delete once again: we should get an error.
+        (is (= (rd/delete-remote-object! ld-id rd-id [:analog-input 1])
+               {:error {:error-class :object :error-code :unknown-object}}))
 
-      ;;   ;; then we delete once again: we should get an error.
-      ;;   (is (= (rd/delete-remote-object! ld-id rd-id [:analog-input 1])
-      ;;          {:error {:error-class :object :error-code :unknown-object}}))
+        ;; ;; create the new object
+        ;; (is (:success (rd/create-remote-object! ld-id rd-id {:object-identifier [:analog-input 1]
+        ;;                                                      :object-name "Test analog"
+        ;;                                                      :description "This is a test"})))
+      
+        ;; ;; set the remote property
+        ;; (rd/set-remote-property! ld-id rd-id [:analog-input 1] :present-value 10)
 
-      ;;   ;; create the new object
-      ;;   (is (:success (rd/create-remote-object! ld-id rd-id {:object-identifier [:analog-input 1]
-      ;;                                                        :object-name "Test analog"
-      ;;                                                        :description "This is a test"})))
-        
-      ;;   ;; set the remote property
-      ;;   (rd/set-remote-property! ld-id rd-id [:analog-input 1] :present-value 10)
+        ;; ;; read the remote property; should have the value we just set.
+        ;; (is (= (-> (rp/read-properties ld-id rd-id [[[:analog-input 1] :present-value]]) 
+        ;;            first
+        ;;            (get :present-value))
+        ;;        10.0))
+        ;; (rd/delete-remote-object! ld-id rd-id [:analog-input 1])        
+      
+        ;; (is (= (-> (rp/read-properties ld-id rd-id [[[:analog-input 1] :present-value]])
+        ;;            (first)
+        ;;            :present-value)
+        ;;        {:error {:error-class :object :error-code :unknown-object}}))
+        )
 
-      ;;   ;; read the remote property; should have the value we just set.
-      ;;   (is (= (-> (rp/read-properties ld-id rd-id [[[:analog-input 1] :present-value]]) 
-      ;;              first
-      ;;              (get :present-value))
-      ;;          10.0))
-      ;;   (rd/delete-remote-object! ld-id rd-id [:analog-input 1])        
-        
-      ;;   (is (= (-> (rp/read-properties ld-id rd-id [[[:analog-input 1] :present-value]])
-      ;;              (first)
-      ;;              :present-value)
-      ;;          {:error {:error-class :object :error-code :unknown-object}})))
+      ;; (testing "Splitting max read multiple references"
+      ;;   ;; create a bunch of objects
+      ;;   (doseq [i (range 10)]          
+      ;;     (rd/create-remote-object! ld-id rd-id {:object-identifier [:analog-input i]
+      ;;                                            :object-name (str "Analog "i)
+      ;;                                            :description "This is a test"}))
 
-      (testing "Splitting max read multiple references"
-        ;; create a bunch of objects
-        (doseq [i (range 10)]          
-          (rd/create-remote-object! ld-id rd-id {:object-identifier [:analog-input i]
-                                                 :object-name (str "Analog "i)
-                                                 :description "This is a test"}))
+      ;;   (let [mrmr (.getMaxReadMultipleReferences (rd/rd ld-id rd-id))]
+      ;;     (.setMaxReadMultipleReferences (rd/rd ld-id rd-id) 2) ;; <--
+      ;;     ;; maximum 2 references this will force us to read the
+      ;;     ;; properties by sending multiple requests. They should all
+      ;;     ;; be merged once they come back.
+      ;;     (let [results (rp/read-properties ld-id rd-id [[[:analog-input 0] :object-name]
+      ;;                                                    [[:analog-input 1] :object-name]
+      ;;                                                    [[:analog-input 3] :object-name]
+      ;;                                                    [[:analog-input 4] :object-name]
+      ;;                                                    [[:analog-input 15] :object-name]] ;; <-- will return an error
+      ;;                                       )]
+      ;;                                   ;(prn results)
+      ;;       (is (= 5 (count results)))
+      ;;       (is (:error-code (:object-name (last results)))))
+      ;;     (.setMaxReadMultipleReferences (rd/rd ld-id rd-id) mrmr))
 
-        (let [mrmr (.getMaxReadMultipleReferences (rd/rd ld-id rd-id))]
-          (.setMaxReadMultipleReferences (rd/rd ld-id rd-id) 2) ;; <--
-          ;; maximum 2 references this will force us to read the
-          ;; properties by sending multiple requests. They should all
-          ;; be merged once they come back.
-          (let [results (rp/read-properties ld-id rd-id [[[:analog-input 0] :object-name]
-                                                         [[:analog-input 1] :object-name]
-                                                         [[:analog-input 3] :object-name]
-                                                         [[:analog-input 4] :object-name]
-                                                         [[:analog-input 15] :object-name]] ;; <-- will return an error
-                                            )]
-            ;(prn results)
-            (is (= 5 (count results)))
-            (is (:error-code (:object-name (last results)))))
-          (.setMaxReadMultipleReferences (rd/rd ld-id rd-id) mrmr))
-
-        ;; delete all objects
-        (doseq [i (range 10)]
-          (rd/delete-remote-object! ld-id rd-id [:analog-input i]))))))
+      ;;   ;; delete all objects
+      ;;   (doseq [i (range 10)]
+      ;;     (rd/delete-remote-object! ld-id rd-id [:analog-input i])))
+      )))
