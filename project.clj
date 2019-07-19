7327a23cd628356fba4182199dbcd758d742146c
Frozenlock <frozenlock@gmail.com>
2019-07-19 15:09:40 -0400

    Use our own fork of BACnet4J and avoid version range deps

--- project.clj
+++ project.clj
@@ -6,10 +6,19 @@
   :repositories {"ias-snapshots" "https://maven.mangoautomation.net/repository/ias-snapshot/"
                  "ias-releases" "https://maven.mangoautomation.net/repository/ias-release/"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [com.infiniteautomation/bacnet4j "5.0.0"]
+
+                 ;; we use a fork of bacnet4j until https://github.com/infiniteautomation/BACnet4J/issues/23 is fixed
+                 [org.clojars.frozenlock/bacnet4j "5.0.0-1"
+                  ;; to avoid version range (and repeatability issues), exclude a few dependencies
+                  :exclusions [org.slf4j/slf4j-api lohbihler/sero-scheduler]]
+                 ;; now require a precise version
+                 [org.slf4j/slf4j-api "1.7.26"]
+                 [lohbihler/sero-scheduler "1.1.0"
+                  :exclusions [org.slf4j/slf4j-api]]
+
                  [clj-serial "2.0.5"]
                  [clj-time "0.15.0"]
-                 [org.slf4j/slf4j-log4j12 "1.8.0-beta0"]]
+                 [org.slf4j/slf4j-log4j12 "1.7.26"]]
   :repl-options {:init-ns user}
   :plugins [[lein-codox "0.9.5"]]
   :codox {:doc-paths ["docs"]
